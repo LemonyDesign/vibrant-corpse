c3c5179aeecb70fdeb763e2000028d520fc2d715
Lemony <lemonystudio@gmail.com>
2018-12-19 15:13:05 +0000

    Add React to set-up

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get("/api", (req, res) => {
+  res.send("lll");
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port);
 
-console.log('App is listening on port ' + port);
+console.log("App is listening on port " + port);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("woowo");
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
